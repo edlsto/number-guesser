bc2cabde8f9c07bb5efb911cfc856d07d9fa76da
Edward Stoner <edwardlstoner@gmail.com>
2019-12-03 16:20:11 -0700

    Grab name and guesses from form and display on page

--- main.js
+++ main.js
@@ -35,3 +35,29 @@ clearFormBtn.addEventListener("click", function () {
   submitGuessBtn.setAttribute("disabled", "disabled");
   clearFormBtn.setAttribute("disabled", "disabled");
 });
+
+submitGuessBtn.addEventListener("click", function () {
+  var name1input = document.querySelector('#challenger-1-name-input');
+  var name1 = name1input.value;
+  var guess1input = document.querySelector('#challenger-1-guess-input');
+  var guess1 = guess1input.value;
+  var name2input = document.querySelector('#challenger-2-name-input');
+  var name2 = name2input.value;
+  var guess2input = document.querySelector('#challenger-2-guess-input');
+  var guess2 = guess2input.value;
+  var name1Display = document.querySelector('#challenger-1-name');
+  name1Display.innerText = name1;
+  var guess1Display = document.querySelector('#challenger-1-guess');
+  guess1Display.innerText = guess1;
+  var name2Display = document.querySelector('#challenger-2-name');
+  name2Display.innerText = name2;
+  var guess2Display = document.querySelector('#challenger-2-guess');
+  guess2Display.innerText = guess2;
+  name1input.value = "";
+  guess1input.value = "";
+  name2input.value = "";
+  guess2input.value = "";
+  submitGuessBtn.setAttribute("disabled", "disabled");
+  clearFormBtn.setAttribute("disabled", "disabled");
+
+});

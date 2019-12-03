a10e209cbcc1410fc3e97f205832fcaabc6e5102
Edward Stoner <edwardlstoner@gmail.com>
2019-12-03 15:47:40 -0700

    Add clear form validation and functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var nameGuessInputs = document.querySelectorAll('.name-guess');
 var submitGuessBtn = document.querySelector('.submit-guess-button');
+var clearFormBtn = document.querySelector('.clear-form-button');
 
 for (var i = 0; i < nameGuessInputs.length; i++) {
   nameGuessInputs[i].addEventListener("keyup", function(){
@@ -14,3 +15,23 @@ for (var i = 0; i < nameGuessInputs.length; i++) {
     }
   });
 }
+
+for (var i = 0; i < nameGuessInputs.length; i++) {
+  nameGuessInputs[i].addEventListener("keyup", function(){
+    if ((nameGuessInputs[0].value != "") ||
+    (nameGuessInputs[1].value != "") ||
+    (nameGuessInputs[2].value != "") ||
+    (nameGuessInputs[3].value != "")
+    ){
+      clearFormBtn.removeAttribute("disabled");
+    }
+  });
+}
+
+clearFormBtn.addEventListener("click", function () {
+  for (var i = 0; i< nameGuessInputs.length; i++) {
+    nameGuessInputs[i].value = "";
+  }
+  submitGuessBtn.setAttribute("disabled", "disabled");
+  clearFormBtn.setAttribute("disabled", "disabled");
+});

7ebddc19d5eb5fedc0f44cdf8d657c8fe2a61b56
Edward Stoner <edwardlstoner@gmail.com>
2019-12-04 17:04:00 -0700

    Move query selectors to global

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ var nameGuessInputs = document.querySelectorAll('.name-guess');
 var submitGuessBtn = document.querySelector('.submit-guess-button');
 var clearFormBtn = document.querySelector('.clear-form-button');
 
+var name1input = document.querySelector('#challenger-1-name-input');
+var guess1input = document.querySelector('#challenger-1-guess-input');
+var name2input = document.querySelector('#challenger-2-name-input');
+var guess2input = document.querySelector('#challenger-2-guess-input');
+var name1Display = document.querySelector('#challenger-1-name');
+var guess1Display = document.querySelector('#challenger-1-guess');
+var name2Display = document.querySelector('#challenger-2-name');
+var guess2Display = document.querySelector('#challenger-2-guess');
+
+
 for (var i = 0; i < nameGuessInputs.length; i++) {
   nameGuessInputs[i].addEventListener("keyup", function(){
     if ((nameGuessInputs[0].value != "") &&
@@ -37,21 +47,13 @@ clearFormBtn.addEventListener("click", function () {
 });
 
 submitGuessBtn.addEventListener("click", function () {
-  var name1input = document.querySelector('#challenger-1-name-input');
   var name1 = name1input.value;
-  var guess1input = document.querySelector('#challenger-1-guess-input');
   var guess1 = guess1input.value;
-  var name2input = document.querySelector('#challenger-2-name-input');
   var name2 = name2input.value;
-  var guess2input = document.querySelector('#challenger-2-guess-input');
   var guess2 = guess2input.value;
-  var name1Display = document.querySelector('#challenger-1-name');
   name1Display.innerText = name1;
-  var guess1Display = document.querySelector('#challenger-1-guess');
   guess1Display.innerText = guess1;
-  var name2Display = document.querySelector('#challenger-2-name');
   name2Display.innerText = name2;
-  var guess2Display = document.querySelector('#challenger-2-guess');
   guess2Display.innerText = guess2;
 
 });

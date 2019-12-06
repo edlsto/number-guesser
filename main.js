6c96e9e73ad2049a72c911d024cb8eab678d4546
Edward Stoner <edwardlstoner@gmail.com>
2019-12-06 15:56:00 -0700

    Restore evaluate function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var nameGuessInputs = document.querySelectorAll('.name-guess');
 var submitGuessBtn = document.querySelector('.submit-guess-button');
 var clearFormBtn = document.querySelector('.clear-form-button');
@@ -18,15 +19,11 @@ var minDisplay = document.querySelector('#min-display');
 var maxDisplay = document.querySelector('#max-display');
 var minNumber = 1;
 var maxNumber = 100;
-
 function calculateMaxMinRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 var number = calculateMaxMinRandom(minNumber, maxNumber);
-
-updateBtn.addEventListener('click', setMinMax);
-
+updateBtn.addEventListener('click', setMinMax)
 function setMinMax () {
   minNumber = parseInt(minInput.value);
   maxNumber = parseInt(maxInput.value);
@@ -34,17 +31,6 @@ function setMinMax () {
   minDisplay.innerText = minNumber;
   maxDisplay.innerText = maxNumber;
 }
-
-var name1input = document.querySelector('#challenger-1-name-input');
-var guess1input = document.querySelector('#challenger-1-guess-input');
-var name2input = document.querySelector('#challenger-2-name-input');
-var guess2input = document.querySelector('#challenger-2-guess-input');
-var name1Display = document.querySelector('#challenger-1-name');
-var guess1Display = document.querySelector('#challenger-1-guess');
-var name2Display = document.querySelector('#challenger-2-name');
-var guess2Display = document.querySelector('#challenger-2-guess');
-var response1Display = document.querySelector('#response1');
-var response2Display = document.querySelector('#response2');
 
 for (var i = 0; i < nameGuessInputs.length; i++) {
   nameGuessInputs[i].addEventListener("keyup", function(){
@@ -59,7 +45,6 @@ for (var i = 0; i < nameGuessInputs.length; i++) {
     }
   });
 }
-
 for (var i = 0; i < nameGuessInputs.length; i++) {
   nameGuessInputs[i].addEventListener("keyup", function(){
     if ((nameGuessInputs[0].value != "") ||
@@ -71,7 +56,6 @@ for (var i = 0; i < nameGuessInputs.length; i++) {
     }
   });
 }
-
 clearFormBtn.addEventListener("click", function () {
  for (var i = 0; i< nameGuessInputs.length; i++) {
    nameGuessInputs[i].value = "";
@@ -79,23 +63,34 @@ clearFormBtn.addEventListener("click", function () {
  submitGuessBtn.setAttribute("disabled", "disabled");
  clearFormBtn.setAttribute("disabled", "disabled");
 });
-
+function clearContents () {
+  guess1input.value = "";
+  guess2input.value = "";
+  submitGuessBtn.setAttribute("disabled", "disabled");
+  clearFormBtn.setAttribute("disabled", "disabled");
+}
 submitGuessBtn.addEventListener("click", function () {
-  var name1input = document.querySelector('#challenger-1-name-input');
   var name1 = name1input.value;
-  var guess1input = document.querySelector('#challenger-1-guess-input');
   var guess1 = guess1input.value;
-  var name2input = document.querySelector('#challenger-2-name-input');
   var name2 = name2input.value;
-  var guess2input = document.querySelector('#challenger-2-guess-input');
   var guess2 = guess2input.value;
-  var name1Display = document.querySelector('#challenger-1-name');
   name1Display.innerText = name1;
-  var guess1Display = document.querySelector('#challenger-1-guess');
   guess1Display.innerText = guess1;
-  var name2Display = document.querySelector('#challenger-2-name');
   name2Display.innerText = name2;
-  var guess2Display = document.querySelector('#challenger-2-guess');
   guess2Display.innerText = guess2;
-
+  clearContents();
+  var response1 = evaluateGuess(guess1, number);
+  var response2 = evaluateGuess(guess2, number);
+  response1Display.innerText = response1;
+  response2Display.innerText = response2;
 });
+//evaluates guess
+function evaluateGuess (guess, number) {
+  if (guess < number) {
+    return "That's too low"
+  } else if (guess > number) {
+    return "That's too high"
+  } else {
+    return "BOOM!"
+  };
+}
